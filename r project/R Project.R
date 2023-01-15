bc9eecfb50947a133c827449d144637fd1033fd2
Kar21thik <117995235+Kar21thik@users.noreply.github.com>
2023-01-15 22:05:52 +0530

    Add files via upload

---
+++ r project/R Project.R
@@ -0,0 +1,326 @@
+library(recommenderlab)
+library(reshape2)
+library(data.table)
+library(ggplot2)
+
+movie_data <- read.csv("~/Custom Office Templates/movies.csv")
+rating_data <- read.csv("~/Custom Office Templates/ratings.csv")
+
+
+
+
+str(movie_data)
+# Overview the summary 
+summary(movie_data)
+head(movie_data)
+summary(rating_data)
+head(rating_data)
+# Data pre-processing
+# Creating a one-hot encoding to create a matrix that comprises of corresponding genres for each of the films.
+movie_genre <- as.data.frame(movie_data$genres, stringsAsFactors=FALSE)
+library(data.table)
+movie_genre2 <- as.data.frame(tstrsplit(movie_genre[,1], '[|]', 
+                                        type.convert=TRUE), 
+                              stringsAsFactors=FALSE) 
+colnames(movie_genre2) <- c(1:10)
+list_genre <- c("Action", "Adventure", "Animation", "Children", 
+                "Comedy", "Crime","Documentary", "Drama", "Fantasy",
+                "Film-Noir", "Horror", "Musical", "Mystery","Romance",
+                "Sci-Fi", "Thriller", "War", "Western")
+genre_mat1 <- matrix(0,10330,18)
+genre_mat1[1,] <- list_genre
+colnames(genre_mat1) <- list_genre
+for (index in 1:nrow(movie_genre2)) {
+  for (col in 1:ncol(movie_genre2)) {
+    gen_col = which(genre_mat1[1,] == movie_genre2[index,col]) 
+    genre_mat1[index+1,gen_col] <- 1
+  }
+}
+genre_mat2 <- as.data.frame(genre_mat1[-1,], stringsAsFactors=FALSE) #remove first row, which was the genre list
+for (col in 1:ncol(genre_mat2)) {
+  genre_mat2[,col] <- as.integer(genre_mat2[,col]) #convert from characters to integers
+} 
+str(genre_mat2)
+
+
+
+
+
+
+
+
+
+# Creating a ‘search matrix’ - searching films by specifying the genre
+SearchMatrix <- cbind(movie_data[,1:2], genre_mat2[])
+head(SearchMatrix)
+ratingMatrix <- dcast(rating_data, userId~movieId, value.var = "rating", na.rm=FALSE)
+ratingMatrix <- as.matrix(ratingMatrix[,-1]) #remove userIds
+
+
+
+#Convert rating matrix into a recommenderlab sparse matrix
+ratingMatrix <- as(ratingMatrix, "realRatingMatrix")
+ratingMatrix
+
+
+
+# Overview some important parameters for building recommendation systems for movies
+recommendation_model <- recommenderRegistry$get_entries(dataType = "realRatingMatrix")
+names(recommendation_model)
+lapply(recommendation_model, "[[", "description")
+
+
+
+# Implementing a single model in the R project – Item Based Collaborative Filtering
+recommendation_model$IBCF_realRatingMatrix$parameters
+# Collaborative Filtering involves suggesting movies to the users that are based on collecting preferences from many other users.
+# With the help of recommenderlab, we can compute similarities between users
+similarity_mat <- similarity(ratingMatrix[1:4, ],
+                             method = "cosine",
+                             which = "users")
+as.matrix(similarity_mat)
+image(as.matrix(similarity_mat), main = "User's Similarities")
+
+
+
+
+
+
+# Portray the similarity that is shared between the films
+movie_similarity <- similarity(ratingMatrix[, 1:4], method =
+                                 "cosine", which = "items")
+as.matrix(movie_similarity)
+image(as.matrix(movie_similarity), main = "Movies similarity")
+rating_values <- as.vector(ratingMatrix@data)
+unique(rating_values) # extracting unique ratings
+Table_of_Ratings <- table(rating_values) # creating a count of movie ratings
+Table_of_Ratings
+
+
+
+
+# Most viewed movies visualization
+library(ggplot2)
+movie_views <- colCounts(ratingMatrix) # count views for each movie
+table_views <- data.frame(movie = names(movie_views),
+                          views = movie_views) # create dataframe of views
+table_views <- table_views[order(table_views$views,
+                                 decreasing = TRUE), ] # sort by number of views
+table_views$title <- NA
+for (index in 1:10325){
+  table_views[index,3] <- as.character(subset(movie_data,
+                                              movie_data$movieId == table_views[index,1])$title)
+}
+table_views[1:6,]
+
+
+
+
+# Visualize a bar plot for the total number of views of the top films
+ggplot(table_views[1:6, ], aes(x = title, y = views)) +
+  geom_bar(stat="identity", fill = 'steelblue') +
+  geom_text(aes(label=views), vjust=-0.3, size=3.5) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  ggtitle("Total Views of the Top Films")
+
+
+
+
+
+
+
+
+
+# Heatmap of Movie Ratings
+# Visualize a heatmap of the movie ratings
+image(ratingMatrix[1:20, 1:25], axes = FALSE, main = "Heatmap of the first 25 rows and 25 columns")
+# Data Preparation
+movie_ratings <- ratingMatrix[rowCounts(ratingMatrix) > 50,
+                              colCounts(ratingMatrix) > 50]
+movie_ratings
+
+
+
+
+
+# describing matrix of relevant users
+minimum_movies<- quantile(rowCounts(movie_ratings), 0.98)
+minimum_users <- quantile(colCounts(movie_ratings), 0.98)
+image(movie_ratings[rowCounts(movie_ratings) > minimum_movies,
+                    colCounts(movie_ratings) > minimum_users],
+      main = "Heatmap of the top users and movies")
+
+
+
+# Visualizing the distribution of the average ratings per user   main code 000000
+average_ratings <- rowMeans(movie_ratings)
+qplot(average_ratings, fill=I("steelblue"), col=I("red")) +
+  ggtitle("Distribution of the average rating per user")
+
+
+
+# Data Normalization
+normalized_ratings <- normalize(movie_ratings)
+sum(rowMeans(normalized_ratings) > 0.00001)
+image(normalized_ratings[rowCounts(normalized_ratings) > minimum_movies,
+                         colCounts(normalized_ratings) > minimum_users],
+      main = "Normalized Ratings of the Top Users")
+
+
+
+# Data Binarization
+binary_minimum_movies <- quantile(rowCounts(movie_ratings), 0.95)
+binary_minimum_users <- quantile(colCounts(movie_ratings), 0.95)
+
+
+
+#movies_watched <- binarize(movie_ratings, minRating = 1)
+good_rated_films <- binarize(movie_ratings, minRating = 3)
+image(good_rated_films[rowCounts(movie_ratings) > binary_minimum_movies,
+                       colCounts(movie_ratings) > binary_minimum_users],
+      main = "Heatmap of the top users and movies")
+# Collaborative Filtering System
+# Splitting the dataset into 80% training set and 20% test set
+sampled_data<- sample(x = c(TRUE, FALSE),
+                      size = nrow(movie_ratings),
+                      replace = TRUE,
+                      prob = c(0.8, 0.2))
+training_data <- movie_ratings[sampled_data, ]
+testing_data <- movie_ratings[!sampled_data, ]
+# Building the Recommendation System
+recommendation_system <- recommenderRegistry$get_entries(dataType ="realRatingMatrix")
+recommendation_system$IBCF_realRatingMatrix$parameters
+recommen_model <- Recommender(data = training_data,
+                              method = "IBCF",
+                              parameter = list(k = 30))
+recommen_model
+class(recommen_model)
+# Exploring the data science recommendation system model
+model_info <- getModel(recommen_model)
+class(model_info$sim)
+dim(model_info$sim)
+top_items <- 20
+image(model_info$sim[1:top_items, 1:top_items],
+      main = "Heatmap of the first rows and columns")
+# Visualize sum of rows and columns with the similarity of the objects above 0
+sum_rows <- rowSums(model_info$sim > 0)
+table(sum_rows)
+sum_cols <- colSums(model_info$sim > 0)
+qplot(sum_cols, fill=I("steelblue"), col=I("red"))+ ggtitle("Distribution of the column count")
+# the number of items to recommend to each user
+top_recommendations <- 10 
+predicted_recommendations <- predict(object = recommen_model,
+                                     newdata = testing_data,
+                                     n = top_recommendations)
+predicted_recommendations
+# recommendation for the first user
+user1 <- predicted_recommendations@items[[1]] 
+movies_user1 <- predicted_recommendations@itemLabels[user1]
+movies_user2 <- movies_user1
+for (index in 1:10){
+  movies_user2[index] <- as.character(subset(movie_data,
+                                             movie_data$movieId == movies_user1[index])$title)
+}
+movies_user2
+# matrix with the recommendations for each user
+recommendation_matrix <- sapply(predicted_recommendations@items,
+                                function(x){ as.integer(colnames(movie_ratings)[x]) }) 
+#dim(recc_matrix)
+recommendation_matrix[,1:4]
+# Distribution of the Number of Items for IBCF
+number_of_items <- factor(table(recommendation_matrix))
+chart_title <- "Distribution of the Number of Items for IBCF"
+qplot(number_of_items, fill=I("steelblue"), col=I("red")) + ggtitle(chart_title)
+
+number_of_items_sorted <- sort(number_of_items, decreasing = TRUE)
+number_of_items_top <- head(number_of_items_sorted, n = 4)
+table_top <- data.frame(as.integer(names(number_of_items_top)),
+                        number_of_items_top)
+for(i in 1:4) {
+  table_top[i,1] <- as.character(subset(movie_data,
+                                        movie_data$movieId == table_top[i,1])$title)
+}
+
+colnames(table_top) <- c("Movie Title", "No. of Items")
+head(table_top)
+
+
+#plot class in r lab
+plot(movie_data)
+str(movie_data)
+plot(movie_data)
+
+
+movie_data
+barchart<-ggplot()
+barchart<- barchart+geom_col(data=movie_data,aes(x=genre,y=number,fill=genre),position = "dodge")
+barplot
+
+
+
+
+head(movie_data$genres)
+sum(duplicated(movie_data))
+library(stringi)
+movie_data$title<-gsub("A^","",as.character(factor(movie_data$title)))
+stri_trim(movie_data$title,side="right")
+
+
+
+
+
+
+
+genres.df <- as.data.frame(movie_data[,c("genres", "title")])
+# separate different genres into new columns
+genres.df$Action <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Action") 1 else 0)
+genres.df$Adventure <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Adventure") 1 else 0)
+genres.df$Animation <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Animation") 1 else 0)
+genres.df$Biography <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Biography") 1 else 0)
+genres.df$Comedy <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Comedy") 1 else 0)
+genres.df$Crime <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Crime") 1 else 0)
+genres.df$Documentary <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Documentary") 1 else 0)
+genres.df$Drama <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Drama") 1 else 0)
+genres.df$Family <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Family") 1 else 0)
+genres.df$Fantasy <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Fantasy") 1 else 0)
+genres.df$`Film-Noir` <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Film-Noir") 1 else 0)
+genres.df$History <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "History") 1 else 0)
+genres.df$Horror <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Horror") 1 else 0)
+genres.df$Musical <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Musical") 1 else 0)
+genres.df$Mystery <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Mystery") 1 else 0)
+genres.df$News <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "News") 1 else 0)
+genres.df$Romance <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Romance") 1 else 0)
+genres.df$`Sci-Fi` <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Sci-Fi") 1 else 0)
+genres.df$Short <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Short") 1 else 0)
+genres.df$Sport <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Sport") 1 else 0)
+genres.df$Thriller <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Thriller") 1 else 0)
+genres.df$War <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "War") 1 else 0)
+genres.df$Western <- sapply(1:length(genres.df$genres), function(x) if (genres.df[x,1] %like% "Western") 1 else 0)
+# get the mean of imdb score for different genres
+means <- rep(0,23)
+for (i in 1:23) {
+  means[i] <- mean(genres.df$title[genres.df[i+2]==1])
+}
+# plot the means
+barplot(means, main = "Average imdb scores for different genres")
+
+
+
+ggplot(movie_data, aes(title)) +
+  geom_bar() +
+  labs(x = "Year movie was released", y = "Movie Count", title = "Histogram of Movie released") +
+  theme(plot.title = element_text(hjust = 0.5))
+movie_data<- movie_data[movie_data$title >= 1990,]
+
+
+plot(movie_data,type="o")
+
+
+summary(movie_data)
+movie_data$genres
+str(movie_data)
+
+plot(movie_data)
+pie(rating_data)
+
+
